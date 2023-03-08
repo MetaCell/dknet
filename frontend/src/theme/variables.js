74ad845d5095fe4fed17fd178bc6b76243a4ae86
Aiga115 <aiguljhyldyzbekkyzy@gmail.com>
2023-03-08 11:43:18 +0100

    #DK2-76 theming added

---
+++ frontend/src/theme/variables.js
@@ -0,0 +1,26 @@
+export const vars = {
+  primaryFont: "Figtree, sans-serif",
+  white: '#FFFFFF',
+
+  grey200: '#EAECF0',
+  grey400: '#98A2B3',
+  grey500: '#667085',
+  grey600: '#475467',
+  grey700: '#344054',
+  grey800: '#1D2939',
+  grey900: '#101828',
+  gray50:'#F9FAFB',
+
+  primary600: '#0BA47D',
+  primary700: '#088E75',
+  primary50: '#D1F2DF',
+
+  error700: '#B42318',
+  error500: '#F04438',
+
+  warning700: '#B54708',
+  warning500: '#F79009',
+
+  success700: '#027A48',
+  success500: '#12B76A',
+};
