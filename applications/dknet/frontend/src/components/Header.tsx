76627f59e35c34e3458d13a61400a4d34880badf
Aiga115 <aiguljhyldyzbekkyzy@gmail.com>
2023-03-31 16:02:29 +0200

    #DK2-89 small change

--- applications/dknet/frontend/src/components/Header.tsx
+++ applications/dknet/frontend/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 //components
 import AppBar from '@mui/material/AppBar';
@@ -7,6 +7,7 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
+import AboutDialog from "./AboutDialog/AboutDialog";
 
 //icons
 import { LogoIcon } from '../assets/icons';
@@ -14,7 +15,15 @@ import { LogoIcon } from '../assets/icons';
 
 
 const Header = () => {
+  const [ openAboutDialog, setOpenDialogWindow ] = useState(false);
 
+  const handleOpen = () => {
+    setOpenDialogWindow(true);
+  };
+
+  const handleClose = () => {
+    setOpenDialogWindow(false);
+  };
 
   return (
     <AppBar position="static" sx={{ background: 'transparent', boxShadow: 'none' }}>
@@ -30,7 +39,8 @@ const Header = () => {
 
             <Box sx={{  display: 'flex' }}>
               <Button variant="text">Send us feedback</Button>
-              <Button variant="contained">About Dknet Repo</Button>
+              <Button variant="contained" onClick={handleOpen}>About Dknet Repo</Button>
+              <AboutDialog open={openAboutDialog} onClose={handleClose}/>
             </Box>
 
           </Box>
