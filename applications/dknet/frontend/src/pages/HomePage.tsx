3b80f491fce6d549971193636736157535a8af5a
Salam Dalloul <salam@leannode.com>
2023-03-31 15:55:50 +0200

    Merge branch 'develop' of https://github.com/MetaCell/dknet into DK2-86
    # Conflicts:
    #	applications/dknet/frontend/src/App.tsx
    #	applications/dknet/frontend/src/pages/Questionnaire.tsx

--- applications/dknet/frontend/src/pages/HomePage.tsx
+++ applications/dknet/frontend/src/pages/HomePage.tsx
@@ -4,11 +4,22 @@ import React from 'react';
 import withLayout from "../useLayout";
 
 import MainLayout from "../components/Layouts/Main";
+import Grid from "@mui/material/Grid";
+import Filters from "../components/Filters";
+import Container from "@mui/material/Container";
 
 
 const HomePage = () => {
   return (
-    <div></div>
+    <Container>
+      <Grid container spacing={2}>
+        <Grid item xs={7}>
+        </Grid>
+        <Grid item xs={5}>
+          <Filters />
+        </Grid>
+      </Grid>
+    </Container>
   )
 }
 const HomePageWithLayout = withLayout(MainLayout)(HomePage);
