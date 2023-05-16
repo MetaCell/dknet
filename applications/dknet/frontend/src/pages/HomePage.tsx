e99d044a49268a947f675cb81823ef3efbcf318e
Salam Dalloul <salam@leannode.com>
2023-05-16 20:35:32 +0200

    Merge branch 'develop' of https://github.com/MetaCell/dknet into DK2-109
    # Conflicts:
    #	applications/dknet/frontend/src/pages/HomePage.tsx

--- applications/dknet/frontend/src/pages/HomePage.tsx
+++ applications/dknet/frontend/src/pages/HomePage.tsx
@@ -48,37 +48,9 @@ const HomePage = () => {
 
   return (
     <Container>
-      <Grid container spacing={2}>
-        <Grid md={8.5} item>
-          <Grid spacing={2}>
-            <Grid item display='flex' justifyContent='space-between' alignItems='center' mb={2}>
-              <Typography variant='h5' lineHeight='unset' color='grey.900'>{repositories.length} repositories matching your criteria</Typography>
-              <SortWidget/>
-            </Grid>
-            {
-              repositories && repositories.map((repository, index) => <Grid item key={index}  xs={12} justifyContent='flex-end'>
-                <RepositoryCard key={repository.code} repository={repository} />
-              </Grid>)
-            }
-          </Grid>
-        </Grid>
-        <Grid md={3.5} item>
-          <Stack spacing={2}>
-            <Filters />
-            <Box sx={{
-              background: '#FCFCFD',
-              borderRadius: '12px',
-              padding: 3
-            }}>
-              <Stack spacing={2}>
-                <Typography variant='h5'>Want to learn more on how we show you results?</Typography>
-                <Typography variant='h4'>Discover the rules and algorithms that show you results that you see.</Typography>
-                <Typography variant='h5'>Learn more</Typography>
-              </Stack>
-            </Box>
-          </Stack>
-        </Grid>
-      </Grid>
+      {
+        isFiltersEmpty ? <LaunchPage /> : <RepositoriesList />
+      }
     </Container>
   )
 }
