40217d9b391e413b67bcafadb156a2b4a8b377df
vidhya-metacell <vidhya@metacell.us>
2024-08-08 20:30:12 +0200

    #dk2-198 Radio filter, checkbox style updates according to latest figma

--- applications/dknet/frontend/src/components/widgets/RadioWidget.tsx
+++ applications/dknet/frontend/src/components/widgets/RadioWidget.tsx
@@ -1,45 +1,49 @@
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
-import Radio, { RadioProps } from '@mui/material/Radio';
+import Radio, { type RadioProps } from '@mui/material/Radio';
 import CustomFormControlLabel from "./CustomFormControlLabel";
+import { vars } from '../../theme/variables.js'
+
+const {
+  checkboxBorderColor,
+  primary50,
+  primary600,
+  white
+} = vars;
 
 const BpIcon = styled('span')(({ theme }) => ({
   borderRadius: '50%',
-  width: 16,
-  height: 16,
-  boxShadow:'inset 0 0 0 1px #98A2B3, inset 0 -1px 0 #98A2B3',
-  backgroundColor: '#fff',
-  backgroundImage:
-    theme.palette.mode === 'dark'
-      ? 'linear-gradient(180deg,hsla(0,0%,100%,.05),hsla(0,0%,100%,0))'
-      : 'linear-gradient(180deg,hsla(0,0%,100%,.8),hsla(0,0%,100%,0))',
+  width: 20,
+  height: 20,
+  boxShadow:`inset 0 0 0 0.0625rem ${checkboxBorderColor}, inset 0 -1px 0 ${checkboxBorderColor}`,
+  backgroundColor: white,
   '.Mui-focusVisible &': {
-    outline: '2px auto rgba(19,124,189,.6)',
+    outline: `0.125rem auto ${primary50}`,
     outlineOffset: 2,
   },
   'input:hover ~ &': {
-    backgroundColor: theme.palette.mode === 'dark' ? '#30404d' : '#ebf1f5',
+    backgroundColor: primary50,
+    boxShadow: `inset 0 0 0 0.0625rem ${primary600}, inset 0 -0.0625rem 0 ${primary600}`,
   },
   'input:disabled ~ &': {
     boxShadow: 'none',
-    background:
-      theme.palette.mode === 'dark' ? 'rgba(57,75,89,.5)' : 'rgba(206,217,224,.5)',
+    background: checkboxBorderColor,
   },
 }));
 
 const BpCheckedIcon = styled(BpIcon)({
-  backgroundColor: '#D1F2DF',
+  backgroundColor: primary50,
   backgroundImage: 'linear-gradient(180deg,hsla(0,0%,100%,.1),hsla(0,0%,100%,0))',
-  boxShadow:'inset 0 0 0 1px #0BA47D, inset 0 -1px 0 #0BA47D',
+  boxShadow: `inset 0 0 0 1px ${primary600}, inset 0 -1px 0 ${primary600}`,
   '&:before': {
     display: 'block',
-    width: 16,
-    height: 16,
-    backgroundImage: 'radial-gradient(#0BA47D,#0BA47D 28%,transparent 32%)',
+    width: 20,
+    height: 20,
+    backgroundImage: `radial-gradient(${primary600},${primary600} 28%,transparent 32%)`,
     content: '""',
   },
   'input:hover ~ &': {
-    backgroundColor: '#D1F2DF',
+    backgroundColor: primary50,
   },
 });
 
